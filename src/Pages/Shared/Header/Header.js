d09dd74833d6d41ec305580ae56ab56840ca8cf7
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-08 22:01:10 +0600

    footer added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,11 @@
 import { Button, Navbar } from 'flowbite-react';
 import React from 'react';
+import './Hearder.css'
 
 const Header = () => {
     return (
         <div>
-            <Navbar className='bg-[#031A06] text-white font-bold bg-transparent'>
+            <Navbar className='header text-white font-bold bg-transparent'>
                 <Navbar.Brand className='pl-5 lg:pl-32 lg:py-4'>
                     <div className='text-3xl'>
                         Trainer<span className='font-caveat text-[45px] text-[#FDBF1D]'>X</span>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,11 +1,17 @@
 import React, { createContext } from 'react';
+import { app } from '../../firebase/firebase.config';
+import { getAuth } from "firebase/auth";
 
 
 const AuthContext = createContext();
-
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+
+    const authInfo = {
+
+    }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
