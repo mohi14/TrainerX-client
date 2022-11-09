b87b59ba87759a6fcfea3063457ad92369e3eabc
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-09 19:07:20 +0600

    routes for service using params done

--- src/Pages/Home/ServicesHome/ServicesCard.js
+++ src/Pages/Home/ServicesHome/ServicesCard.js
@@ -1,6 +1,7 @@
 import { Button, Card } from 'flowbite-react';
 import React from 'react';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { Link } from 'react-router-dom';
 
 const ServicesCard = ({ services }) => {
     const { _id, title, image_url, details } = services
@@ -18,8 +19,11 @@ const ServicesCard = ({ services }) => {
                 <p className="font-normal text-gray-700 dark:text-gray-400">
                     {details.slice(0, 100) + '...'}
                 </p>
-                <Button
-                    color={'success'}>View Details</Button>
+                <Link to={`/services/${_id}`}>
+                    <Button
+                        color={'success'}
+                        className='w-full'>View Details</Button>
+                </Link>
             </Card>
         </div>
     );
